30b3bf0b53a1197db409f3f71eabc25a6c17ec0b
altfnsssa-cmd <altfnsssa@gmail.com>
2026-09-02 21:27:22 +0800

    Create Day01.java

---
+++ Day01.java
@@ -0,0 +1,7 @@
+public class Main{
+    public static void main(String[] args){
+        // 1.Komentar 1 baris merupakan komentar yang hanya digunakan untuk satu baris kode
+        /* 2.Komentar beberapa baris merupakan komentar yang bisa digunakan untuk beberapa baris */
+        /*** 3.Komentar dokumentasi merupakan komentar untuk memberikan dokumentasi pada program ***/
+    }
+}
