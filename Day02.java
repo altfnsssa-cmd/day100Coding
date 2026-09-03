d70c2463ece517c7a87f2083aec2222e95937923
altfnsssa-cmd <altfnsssa@gmail.com>
2026-09-03 21:20:07 +0800

    Day02.java

---
+++ Day02.java
@@ -0,0 +1,22 @@
+public class main {
+public static void main(String[] args) {
+    // Menggunakan println()
+    System.out.println("BIODATA DIRI");
+    System.out.println("Nama            = Althafunnisa");
+    System.out.println("Tempat lahir    = tinambung");
+    System.out.println("Jenis kelamin   = Perempuan");
+
+
+    //Mengggunakan print()
+    System.out.print("Kelas           = A\n");
+    System.out.print("Prodi           = Informatika\n");
+
+
+    //Menggunakan printf()
+    String kota = "Kabupaten mamuju";
+    int umur = 18;
+    System.out.printf("Kota            = %s%n" , kota);
+    System.out.printf("Umur            = %d tahun%n" , umur);
+    
+    }
+}
