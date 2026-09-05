f9da978897104865b1b348ef7a2b0d81c370c9e3
altfnsssa-cmd <altfnsssa@gmail.com>
2026-09-05 08:38:02 +0800

    Day04.java

---
+++ Day04.java
@@ -0,0 +1,21 @@
+public class Day4 {
+    public static void main(String[] args) {
+
+        //byte digunakan untuk menyimpan bilangan bulat yang kecil
+        //short digunakan untuk menyimpan bilangan bulat yang lebih besar daripada byte
+        //int adalah tipe data bilangan bulat yang paling sering digunakan dalam Java
+        //long digunakan untuk menyimpan bilangan bulat yang sangat besar
+
+        byte umur = 18;
+        short tahun = 2026;
+        int harga = 80000;
+        long penduduk = 2000000000;
+
+        System.out.println("Umur: " + umur);
+        System.out.println("Tahun: " + tahun);
+        System.out.println("Harga: " + harga);
+        System.out.println("Jumlah penduduk: " + penduduk);
+
+
+    }
+}
