47c7b1ab13d2672e64f39fb4d6ad418463644445
altfnsssa-cmd <altfnsssa@gmail.com>
2026-09-06 20:12:28 +0800

    Day05.java

---
+++ Day05.java
@@ -0,0 +1,14 @@
+blic class day5 {
+    public static void main(String[] args) {
+
+        // float untuk menyimpan angka pecahan,tetapi ketelitiannya lebih rendah
+        float nilai = 85.5f;
+
+        // double juga untuk angka pecahan,tetapi lebih teliti daripada float
+        double harga = 12500.75;
+
+        System.out.println("Nilai: " + nilai);
+        System.out.println("Harga: " + harga);
+
+    }
+}
