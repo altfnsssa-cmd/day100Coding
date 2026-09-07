05ebea386d858d577ec663cdd567e8c106291bd3
altfnsssa-cmd <altfnsssa@gmail.com>
2026-09-07 18:01:58 +0800

    day06.java

---
+++ day06.java
@@ -0,0 +1,18 @@
+public class day6 {
+    public static void main(String[] args) {
+
+        //char digunakan untuk menyimpan satu karakter saja
+        char huruf = 'C';
+        char angka = '8';
+
+        //boolean digunakan untuk menyimpan nilai benar atau salah
+        boolean aktif = true;
+        boolean hujan = false;
+
+        System.out.println("huruf");
+        System.out.println("angka");
+        System.out.println("aktif");
+        System.out.println("hujan");
+
+    }
+}
