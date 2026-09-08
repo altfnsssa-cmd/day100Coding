818a4ff374394c344bd9823183f08fb4c1fd4d0c
altfnsssa-cmd <altfnsssa@gmail.com>
2026-09-08 18:26:51 +0800

    Day07.java

---
+++ Day07.java
@@ -0,0 +1,15 @@
+public class day7 {
+    public static void main(String[] args) {
+        // String adalah tipe data yang digunakan untuk menyimpan teks atau kumpulan
+        // karakter
+
+        String nama = "Caca";
+        String kota = "Mamuju";
+        String pesan = "u can do it";
+
+        System.out.println("Nama: " + nama);
+        System.out.println("Kota: " + kota);
+        System.out.println("Pesan: " + pesan);
+
+    }
+}
