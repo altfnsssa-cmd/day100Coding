ac0f5344fb67b8ba402b6e99efecc7cc39a04bf0
altfnsssa-cmd <altfnsssa@gmail.com>
2026-09-09 19:51:57 +0800

    Day08.java

---
+++ Day08.java
@@ -0,0 +1,16 @@
+public class day8 {
+    public static void main(String[] args) {
+        // Deklarasi membuat variabel dan menentukan tipe datanya
+        int umur;
+
+        // Inisialisasi memberikan nilai awal pada variabel
+        umur = 17;
+
+        // Update mengubah nilai variabel yang sudah ada
+        umur = 18;
+
+        System.out.println("Sebelum diubah: " + umur);
+        System.out.println("Sesudah diubah: " + umur);
+
+    }
+}
