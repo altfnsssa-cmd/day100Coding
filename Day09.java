b1b5af54431b4ef9700d3e9d72723f8e31ec469c
altfnsssa-cmd <altfnsssa@gmail.com>
2026-09-10 21:03:41 +0800

    Day09.java

---
+++ Day09.java
@@ -0,0 +1,13 @@
+public class day9 {
+    public static void main(String[] args) {
+
+        // konstanta adalah variabel yang nilainya tidak dapat diubah setelah diberikan
+        // nilai pertama kali
+        final double PI = 3.14;
+        int jarijari = 7;
+
+        double luas = PI * jarijari * jarijari;
+        System.out.println("Luas lingkaran = " + luas);
+
+    }
+}
