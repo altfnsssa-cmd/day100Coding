788ae4d5202160f4e13ed90d1a3bdce236271065
altfnsssa-cmd <altfnsssa@gmail.com>
2026-09-11 16:16:03 +0800

    Day10.java

---
+++ Day10.java
@@ -0,0 +1,18 @@
+public class day10 {
+    public static void main(String[] args) {
+
+        // Batas minimal dan maksimal tipe data adalah nilai terkecil dan terbesar yang
+        // dapat disimpan
+        // oleh suatu tipe data dalam program. Setiap tipe data memiliki kapasitas
+        // penyimpanan yang berbeda
+
+        byte angka1 = -128; // Batas minimal
+        byte angka2 = 127; // Batas maksimal
+        byte angka3 = 50; // Nilai di antara batas
+
+        System.out.println("angka1: "+ angka1);
+        System.out.println("angka2: "+ angka2);
+        System.out.println("angka3: "+ angka3);
+
+    }
+}
