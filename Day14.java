aa46aeda96f05c844fde567db00c7988ab8d20d6
altfnsssa-cmd <altfnsssa@gmail.com>
2026-09-15 20:12:22 +0800

    Day14.java

---
+++ Day14.java
@@ -0,0 +1,13 @@
+public class day13 {
+    public static void main(String[] args) {
+
+        int angka1 = 18;
+        int angka2 = 10;
+
+        int penjumlahan = angka1 + angka2;
+        int pengurangan = angka1 - angka2;
+
+        System.out.println("penjumlahan: " + penjumlahan);
+        System.out.println("pengurangan: " + pengurangan);
+    }
+}
