6ab00be49fe3c28dcc8d3b6f5738d86283017913
altfnsssa-cmd <altfnsssa@gmail.com>
2026-09-16 20:29:34 +0800

    Day15.java

---
+++ Day15.java
@@ -0,0 +1,16 @@
+public class day15 {
+    public static void main(String[] args) {
+
+        int angka1 = 25;
+        int angka2 = 12;
+
+        int hasilPerkalian = angka1 * angka2;
+        int hasilPembagian = angka1 / angka2;
+
+        System.out.println("Angka1: "+ angka1);
+        System.out.println("Angka2: "+ angka2);
+
+        System.out.println("hasilPerkalian: "+ hasilPerkalian);
+        System.out.println("hasilPembagian: "+ hasilPembagian);     
+    }
+}
